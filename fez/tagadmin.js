e3683242a727457e9160db1fe115b7c0ddb34a18
Next5x_timnasa <botuse41@gmail.com>
2025-05-20 10:07:59 +0300

    Create tagadmin.js

---
+++ fez/tagadmin.js
@@ -0,0 +1,44 @@
+const { timoth } = require("../timnasa/timoth")
+//const { getGroupe } = require("../bdd/groupe")
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
+const {ajouterOuMettreAJourJid,mettreAJourAction,verifierEtatJid} = require("../bdd/antilien")
+const {atbajouterOuMettreAJourJid,atbverifierEtatJid} = require("../bdd/antibot")
+const fs = require("fs-extra");
+const conf = require("../set");
+const { default: axios } = require('axios');
+
+
+timoth({ nomCom: "tagadmin", categorie: 'Group', reaction: "🪰" }, async (dest, zk, commandeOptions) => {
+
+  const { ms, repondre, arg, verifGroupe, nomGroupe, infosGroupe, nomAuteurMessage, verifAdmin, superUser } = commandeOptions;
+
+  if (!verifGroupe) { 
+    repondre(" thís cσmmαnd ís rєsєrvєd fσr grσups"); 
+    return; 
+  }
+
+  if (!verifAdmin && !superUser) { 
+    repondre("cσmmαnd ís rєsєrvєd fσr grσups"); 
+    return; 
+  }
+
+  let mess = arg && arg !== ' ' ? arg.join(' ') : 'Aucun Message';
+
+  let adminsGroupe = infosGroupe.participants.filter(membre => membre.admin); // Filtering only admins
+
+  let tag = `  
+*Group :*ᴛɪᴍɴᴀsᴀ ᴛᴀɢᴀᴅᴍɪɴ ${nomGroupe} 
+*Hey :ᴛɪᴍɴᴀsᴀ ᴛᴀɢᴀᴅᴍɪɴ* ${nomAuteurMessage}* 
+*Message :* *${mess}* 
+`;
+
+  let emoji = ['🤔,'⭐,'🎉,'💯', '🖐️'];
+  let random = Math.floor(Math.random() * emoji.length);
+
+  for (const membre of adminsGroupe) {
+    tag += `${emoji[random]} @${membre.id.split("@")[0]}\n`;
+  }
+
+  zk.sendMessage(dest, { text: tag, mentions: adminsGroupe.map(i => i.id) }, { quoted: ms });
+
+});
