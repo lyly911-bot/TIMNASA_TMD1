976f12066214ee441e30db5a89a838daeb525937
Next5x_timnasa <botuse41@gmail.com>
2025-05-22 05:53:17 +0300

    Update forward.js

--- fez/forward.js
+++ fez/forward.js
@@ -1,11 +1,11 @@
-const { zokou } = require("../framework/zokou");
+const { timoth } = require("../timnasa/timoth");
 const { ajouterOuMettreAJourJid, mettreAJourAction, verifierEtatJid } = require("../bdd/antilien");
 const { atbajouterOuMettreAJourJid, atbverifierEtatJid } = require("../bdd/antibot");
 const fs = require("fs-extra");
 const conf = require("../set");
 const { default: axios } = require('axios');
 
-zokou({ nomCom: "forward", categorie: 'Group', reaction: "📤" }, async (dest, zk, commandeOptions) => {
+timoth({ nomCom: "forward", categorie: 'Group', reaction: "📤" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, arg, verifGroupe, nomGroupe, infosGroupe, nomAuteurMessage, superUser  } = commandeOptions;
 
   if (!verifGroupe) {
@@ -23,7 +23,7 @@ zokou({ nomCom: "forward", categorie: 'Group', reaction: "📤" }, async (dest, 
       // Send the message directly to the member's DM
       await zk.sendMessage(memberNumber, { text: mess });
     }
-    repondre('*RAHMANI-MD Forwarded Your Message To All group members in their DMs.*');
+    repondre('*timnasa Forwarded Your Message To All group members in their DMs.*');
   } else {
     repondre('*Only My Owner Can Use This Command.*');
   }
